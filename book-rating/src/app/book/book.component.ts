b05d523c9dc58f60ff2ea68bfa4739f964f58f57
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2019-07-15 16:40:25 +0200

    workshop-styles with 😸

--- book-rating/src/app/book/book.component.ts
+++ book-rating/src/app/book/book.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
 import { Book } from '../shared/book';
 
 @Component({
   selector: 'br-book',
   templateUrl: './book.component.html',
-  styleUrls: ['./book.component.scss']
+  styleUrls: ['./book.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush // Turbo an!
 })
 export class BookComponent {
 
