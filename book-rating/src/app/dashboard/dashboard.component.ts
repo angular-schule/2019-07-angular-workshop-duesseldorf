f9f191e744fe4b36a5247206ab0c03cf107ef7fc
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2019-07-15 14:39:08 +0200

    interfaces instead of classes

--- book-rating/src/app/dashboard/dashboard.component.ts
+++ book-rating/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from '../shared/book';
 
 @Component({
   selector: 'br-dashboard',
@@ -7,12 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  books: string[];
+  books: Book[];
 
   constructor() { }
 
   ngOnInit() {
-    this.books = ['Angular', 'AngularJS', 'React'];
+    this.books = [
+      {
+        isbn: '111',
+        title: 'Angular',
+        description: 'Tolles Buch',
+        rating: 5
+      },
+      {
+        isbn: '222',
+        title: 'AngularJS',
+        description: 'Altes Buch',
+        rating: 3
+      },
+      {
+        isbn: '333',
+        title: 'React',
+        description: 'Blubb',
+        rating: 1
+      }
+    ];
   }
 
 }
