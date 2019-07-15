3f830da1a6369dd4368e8851e9e3c24363bddb66
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2019-07-15 15:08:42 +0200

    property bindings

---
+++ book-rating/src/app/book/book.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { Book } from '../shared/book';
+
+@Component({
+  selector: 'br-book',
+  templateUrl: './book.component.html',
+  styleUrls: ['./book.component.scss']
+})
+export class BookComponent {
+
+  @Input()
+  public book: Book;
+}
