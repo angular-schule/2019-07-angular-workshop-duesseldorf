c44ff55ce4593e629fea65c94055e17548857580
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2019-07-17 13:27:01 +0200

    rxjs: hands on lösung

--- rxjs-playground-node/exercises/6_operators.ts
+++ rxjs-playground-node/exercises/6_operators.ts
@@ -1,17 +1,15 @@
 import { from } from 'rxjs';
-import { map, filter, reduce } from 'rxjs/operators';
+import { map, filter, reduce, scan, tap } from 'rxjs/operators';
 
 let dataArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 
 from(dataArray)
   .pipe(
-    map(z => z + 10)
+    map(z => z + 10),
+    filter(z => z % 2 == 0),
+    // tap(z => console.log(z)),
+    reduce((a, b) => a + b),
+    map(z => '❤'.repeat(z)),
   )
   .subscribe(console.log)
-
-// 1. liefere Zahlen von 1 bis 10
-// 2. addiere diese mit 10
-// 3. filtere alle Zahlen aus, die ungerade sind
-// 4. bilde die Summe aus diesen Zahlen
-// 5. zeige so viele Herzen an, wie die Summe lautet
