24f1a21ae2388032e92f80d275652a2d39efcfab
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2019-07-17 15:01:30 +0200

    rxjs antipattern. not sharing the same observable

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,38 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { map, mergeMap, switchMap, catchError } from 'rxjs/operators';
-import { Book } from '../shared/book';
 import { BookStoreService } from '../shared/book-store.service';
 import { HttpErrorResponse } from '@angular/common/http';
 import { of } from 'rxjs';
-
 
 @Component({
   selector: 'br-book-details',
   templateUrl: './book-details.component.html',
   styleUrls: ['./book-details.component.scss']
 })
-export class BookDetailsComponent implements OnInit {
+export class BookDetailsComponent {
 
-  book: Book;
+  book$ = this.route.paramMap
+    .pipe(
+      map(p => p.get('isbn')),
+      switchMap(isbn => this.bs.getSingle(isbn)
+        .pipe(catchError((error: HttpErrorResponse) => of({
+          isbn: '000',
+          title: 'Es gab einen Fehler',
+          description: error.url,
+          rating: 1,
+          firstThumbnailUrl: ''
+        }))))
+    );
 
   constructor(
     private route: ActivatedRoute,
     private bs: BookStoreService) { }
-
-  ngOnInit() {
-    this.route.paramMap
-      .pipe(
-        map(p => p.get('isbn')),
-        switchMap(isbn => this.bs.getSingle(isbn)
-          .pipe(catchError((error: HttpErrorResponse) => of({
-            isbn: '000',
-            title: 'Es gab einen Fehler',
-            description: error.url,
-            rating: 1,
-            firstThumbnailUrl: ''
-          }))))
-      )
-      .subscribe(book => this.book = book);
-  }
 }
