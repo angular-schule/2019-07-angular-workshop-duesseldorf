f1ef29f62373deb914c53c6c24fdf1ce3581144e
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2019-07-17 10:47:54 +0200

    rxjs done right

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -13,7 +13,8 @@ export class BookDetailsComponent implements OnInit {
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.isbn = this.route.snapshot.paramMap.get('isbn');
+    this.route.paramMap
+      .subscribe(param => this.isbn = param.get('isbn'));
   }
 
 }
