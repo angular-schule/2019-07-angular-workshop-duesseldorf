970eec812259766210955bbc648c7802cb805fdb
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2019-07-18 14:22:28 +0200

    ng add @angular/material

--- book-rating/src/app/app.module.ts
+++ book-rating/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { AppEffects } from './app.effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 import { LoadBooks } from './books/actions/book.actions';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,8 @@ import { LoadBooks } from './books/actions/book.actions';
       }
     }),
     EffectsModule.forRoot([AppEffects]),
-    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
