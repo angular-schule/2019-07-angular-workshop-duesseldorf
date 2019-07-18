dc9552e818044ff5199f8f053e2d093bd121d8d6
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2019-07-18 11:27:23 +0200

    ngrx: actions + reducers

--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@
 import { Book } from '../shared/book';
 import { BookRatingService } from '../shared/book-rating.service';
 import { BookStoreService } from '../shared/book-store.service';
+import { Store } from '@ngrx/store';
+import { State } from 'src/app/reducers';
+import { LoadBooks } from '../actions/book.actions';
 
 @Component({
   selector: 'br-dashboard',
@@ -13,35 +16,33 @@ export class DashboardComponent implements OnInit {
 
   books: Book[] = [];
 
-  constructor(
-    private br: BookRatingService,
-    private bs: BookStoreService,
-    private cd: ChangeDetectorRef) { }
+  constructor(private store: Store<State>) { }
 
   ngOnInit() {
-    this.bs.getAll().subscribe(books => {
-      this.books = books;
-      this.cd.markForCheck();
-    });
+    this.store.dispatch(new LoadBooks());
   }
 
+  // TODO: redux!
   doRateDown(book: Book) {
-    const ratedBook = this.br.rateDown(book);
-    this.updateAndSort(ratedBook);
+    // const ratedBook = this.br.rateDown(book);
+    // this.updateAndSort(ratedBook);
   }
 
+  // TODO: redux!
   doRateUp(book: Book) {
-    const ratedBook = this.br.rateUp(book);
-    this.updateAndSort(ratedBook);
+    // const ratedBook = this.br.rateUp(book);
+    // this.updateAndSort(ratedBook);
   }
 
+  // TODO: redux!
   updateAndSort(ratedBook: Book) {
-    this.books = this.books
-      .map(b => b.isbn === ratedBook.isbn ? ratedBook : b)
-      .sort((a , b) => b.rating - a.rating);
+    // this.books = this.books
+    //   .map(b => b.isbn === ratedBook.isbn ? ratedBook : b)
+    //   .sort((a , b) => b.rating - a.rating);
   }
 
+  // TODO: redux!
   doCreateBook(newBook: Book) {
-    this.books = [...this.books, newBook];
+    // this.books = [...this.books, newBook];
   }
 }
--- book-rating/src/app/books/reducers/book.reducer.ts
+++ book-rating/src/app/books/reducers/book.reducer.ts
@@ -1,25 +1,39 @@
 
 import { BookActions, BookActionTypes } from '../actions/book.actions';
+import { Book } from '../shared/book';
 
 export interface State {
-
+  books: Book[];
+  loading: boolean;
 }
 
 export const initialState: State = {
-
+  books: [],
+  loading: false
 };
 
 export function reducer(state = initialState, action: BookActions): State {
   switch (action.type) {
 
     case BookActionTypes.LoadBooks:
-      return state;
+      return {
+        ...state,
+        loading: true
+      };
 
     case BookActionTypes.LoadBooksSuccess:
-      return state;
+      return {
+        ...state,
+        loading: false,
+        books: action.payload.data
+      };
 
     case BookActionTypes.LoadBooksFailure:
-      return state;
+      return {
+        ...state,
+        loading: false,
+        books: []
+      };
 
     default:
       return state;
